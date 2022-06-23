6a9a11a28d4fd413419be429e7bbe798e71b322e
Niklas Schikora <Niklas.Schikora@gmail.com>
2022-06-23 11:58:49 +0200

    implement basic time control and time circle component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { Center, Container } from "@chakra-ui/react";
 import { Header } from "./components/Header";
+import { TimeCircle } from "./components/TimeCircle";
+import { TimeControl } from "./components/TimeControl";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Container>
+        <Center>
+          <TimeCircle />
+        </Center>
+        <Center>
+          <TimeControl />
+        </Center>
+      </Container>
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
+import { Center, Flex, Heading } from "@chakra-ui/react";
 import React from "react";
+import { TimeControl } from "./TimeControl";
 
 export const Header = () => {
   return (
     <React.Fragment>
-      <p>Header</p>
+      <Center padding="5">
+        <Heading as="h1" size="2xl">
+          pomodo.ro ✨
+        </Heading>
+      </Center>
     </React.Fragment>
   );
 };
